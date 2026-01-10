2213eefd75f7ba657677c89d7b8628cee4858521
BrainWave <pikadon92+1@gmail.com>
2026-01-10 21:36:21 +0800

    Breakdown Field > start(void)

--- src/cutscene.c
+++ src/cutscene.c
@@ -1,12 +1,12 @@
 // for ExecSound_ext
 #include "sound.h"
 
-void showCutscene(void);
+void showCutscene(uint8_t id);
 void decomp(void);
 
 // Address: 000f
 // A: cutscene id
-void showCutscene(void) {}
+void showCutscene(uint8_t id) {}
 
 // Address: 0053
 // Decompress
--- src/field.c
+++ src/field.c
@@ -14,6 +14,27 @@
 #include "sound.h"
 
 void start(void);
+static void fieldLoop(void);
+static void showCutsceneFromField(uint8_t id);
+static void func_c044e3(void);
+static void func_c0450a(void);
+static void func_c04528(void);
+static void func_c048dd(void);
+static void func_c048ed(void);
+static void func_c048fa(void);
+static void func_c0490a(void);
+static void func_c0491d(void);
+static void loadMapNoFade(void);
+static void initInterrupts(void);
+static void initHardware(void);
+static void execEvent(void);
+
+static uint8_t h_memsel = 0;
+static uint8_t h_mdmaen = 0;
+static uint8_t h_hdmaen = 0;
+static uint8_t h_inidisp = 0;
+static uint8_t h_nmitimen = 143;
+static uint8_t addr_7e0134 = 3;
 
 void start(void) {
     // SEt Interrpt flag
@@ -21,15 +42,18 @@ void start(void) {
     // eXchange Carry and Emulation flags
     // Lengthen indexes to 16-bits
     // Shorten Accumulator (A) to 8-bits
+
     // STore Zero to hMEMSEL
+    //  - Used elsewhere?
     // STore Zero to hMDMAEN
     // STore Zero to hHDMAEN
-    // LoaD #$8f to A
+    // LoaD #$8f (143) to A
     // Store A to hINIDISP
     // LoaD #0 to A
     // Store A to hNMITIMEN
+
     // LoaD #0 to A
-    // eXchange B and A 8-bit accumulators
+    // eXchange data Bank and A 8-bit accumulators
     // LoaD #$1fff to X
     // Transfer X to Stack pointer
     // Load #0 to A
@@ -41,22 +65,41 @@ void start(void) {
     // LoaD #0 to X
     // STore X to $06
     // Jump to Subroutine Long InitSound_ext
+    initSound();
+
     // LoaD #$f1 to Accumulator
+    const uint8_t CUTSCENE_TITLE = 241;
     // Jump to SubRoutine ShowCutscene
+    showCutsceneFromField(CUTSCENE_TITLE);
+
     // Jump to SubRoutine InitHardware
+    initHardware();
+
     // Jump to SubRoutine _c0490a
+    func_c0490a();
+
     // LoaD #3 to Accumulator
-    // STore $0134 to A
+    // STore A to $0134
+    //  - Could be set when defined directly.
+
     // Jump to Subroutine Long ExecMenu_ext
+    execMenu();
+
     // Jump to SubRoutine _c044e3
+    func_c044e3();
+
     // Jump to SubRoutine InitHardware
+    initHardware();
+
     // Jump to SubRoutine InitInterrupts
+    initInterrupts();
+
     // LoaD $0139 to A
-    // Branch to [NewGame] if EQual (Zero Flasg is set)
+    // Branch to [NewGame] if EQual (Zero Flag is set)
 
     // (restore saved game)
 
-    // Jump to SubRoutine _c044e3
+    // Jump to SubRoutine _c0491d
     // LoaD $0af9 to A
     // Store A to $0b60
     // Shift Right A
@@ -71,5 +114,58 @@ void start(void) {
     // Load $0ad9 to A (y position)
     // Store A to $1089
     // Jump to subroutine LoadMapNoFade
+    
     // Jump to FieldLoop
+
+    // [NewGame]
+    // Jump to SubRoutine _c048fa
+    // Jump to SubRoutine _c048ed
+    // Jump to SubRoutine _c048dd
+    // Jump to SubRoutine _c04528
+    // Jump to SubRoutine _c0450a
+    // Load #1 to A
+    // Store A to $bd
+    // Store A to $bc
+    // Load #$0010 to X
+    // Store X to $ce
+    // Load #1 to A
+    // Store $57 to A
+    // Load #$81 to A
+    // Store A to hNMITIMEN
+    // cli
+    // Jump to SubRoutine ExecEvent
+    // Store Zero to $57
+    // Store Zero to $58
+    // Store Zero to $59
+    
+    // JuMP to FieldLoop
 }
+
+// Field Main Loop
+static void fieldLoop(void) {}
+
+static void showCutsceneFromField(uint8_t id) {}
+
+static void func_c044e3(void) {}
+
+static void func_c0450a(void) {}
+
+static void func_c04528(void) {}
+
+static void func_c048dd(void) {}
+
+static void func_c048ed(void) {}
+
+static void func_c048fa(void) {}
+
+static void func_c0490a(void) {}
+
+static void func_c0491d(void) {}
+
+static void loadMapNoFade(void) {}
+
+static void initInterrupts(void) {}
+
+static void initHardware(void) {}
+
+static void execEvent(void) {}
