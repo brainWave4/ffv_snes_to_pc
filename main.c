ea60210ae55416891598336a584ecb90a750b7ac
BrainWave <pikadon92+1@gmail.com>
2026-01-27 14:57:41 +0800

    Updated Main

--- main.c
+++ main.c
@@ -1,19 +1,56 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include "field.h"
 
 int main(void) {
-    
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Final Fantasy V", 256, 224, 0);
-    
-    SDL_Event event;
-    while (SDL_WaitEvent(&event)) {
-        if (event.type == SDL_EVENT_QUIT) {
-            break;
-        }
+    // Initializing Window
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENT) > 0) {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError())
+        return -1;
     }
 
+    SDL_Window* window = SDL_CreateWindow("FFV", 256, 224, 0);
+    if (window == NULL) {
+        SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError())
+        return -2;
+    }
+
+    SDL_Renderer renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNCH);
+    if (renderer == NULL) {
+        SDL_Log("SDL_CreateRenderer Error: %s", SDL_GetError())
+        return -3;
+    }
+
+    // Setup
+    //start();
+
+    // Game Loop
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        // Input
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_EVENT_QUIT:
+                running = false;
+                break;
+            }
+        }
+
+        // Inner Logic Loop
+        //fieldLoop();
+
+        // Rendering
+    }
+
+    // Closing
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    
+
     return 0;
 }
