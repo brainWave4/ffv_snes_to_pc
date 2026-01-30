cf9d6a69b38585a1ba34a1ad119c56ad8b22beab
BrainWave <pikadon92+1@gmail.com>
2026-01-30 21:38:17 +0800

    Updated Main

--- main.c
+++ main.c
@@ -1,56 +1,75 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #include "field.h"
 
-int main(void) {
-    // Initializing Window
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    bool running;
+} AppState;
 
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    // Initializing Window
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENT) > 0) {
         SDL_Log("SDL_Init Error: %s", SDL_GetError())
-        return -1;
+        return SDL_APP_FAILURE;
     }
+    
+    AppState* state = (AppState*)SDL_calloc(1, sizeof(AppState));
+    if (!state) return SDL_APP_FAILURE;
 
-    SDL_Window* window = SDL_CreateWindow("FFV", 256, 224, 0);
-    if (window == NULL) {
+    state->window = SDL_CreateWindow("FFV", 256, 224, 0);
+    if (state->window == NULL) {
         SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError())
-        return -2;
+        return SDL_APP_FAILURE;
     }
 
-    SDL_Renderer renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNCH);
-    if (renderer == NULL) {
+    state->renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNCH);
+    if (state->renderer == NULL) {
         SDL_Log("SDL_CreateRenderer Error: %s", SDL_GetError())
-        return -3;
+        return SDL_APP_FAILURE;
     }
+
+    state->running = true;
+    *appstate = state;
 
     // Setup
     //start();
 
-    // Game Loop
+    return SDL_APP_CONTINUE;
+}
 
-    bool running = true;
-    SDL_Event event;
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    // Inner Logic Loop
+    //fieldLoop();
 
-    while (running) {
-        // Input
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_EVENT_QUIT:
-                running = false;
-                break;
-            }
-        }
+    AppState* state = (AppState*)appstate;
 
-        // Inner Logic Loop
-        //fieldLoop();
+    // Rendering
 
-        // Rendering
+    return state->running ? SDL_APP_CONTINUE : SDL_APP_SUCCESS;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    AppState* state = (AppState*)appstate;
+
+    switch (event->type) {
+    case SDL_EVENT_QUIT:
+        state->running = false;
+        break;
     }
 
-    // Closing
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    return SDL_APP_CONTINUE;
+}
 
-    return 0;
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    if (appstate != NULL) {
+        AppState* state = (AppState*)appstate;
+        if (state->renderer) SDL_DestroyRenderer(state->renderer);
+        if (state->window) SDL_DestroyWindow(state->window);
+        SDL_Free(state);
+    }
 }
