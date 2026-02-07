9ce3610a02a5b458aeb1633b2fd1e1aff70b916f
BrainWave <pikadon92+1@gmail.com>
2026-02-07 14:31:58 +0800

    Made Rommap Not a Nested List

--- tools/rom.py
+++ tools/rom.py
@@ -1,15 +1,11 @@
-def readToAddr(file, b_arr):
+def readFromBank(rommap, file):
     for h in range(0x100):
-        h_arr = [];
-        
         for l in range(0x100):
-            h_arr.append(file.read(1))
+            rommap.append(file.read(1))
         
-        b_arr.append(h_arr)
-        
-def mismatchedHeaderTitle(title, rommap, starting_l):
+def mismatchedHeaderTitle(title, rommap, addr):
     for i in range(len(title)):
-        if rommap[0][0xff][starting_l + i] != title[i]:
+        if rommap[addr + i] != title[i]:
             return True
 
     return False
@@ -17,18 +13,16 @@ def mismatchedHeaderTitle(title, rommap, starting_l):
 def getRomMap(ext, filepath, header, title):
     print(f"Opening {ext} file...")
 
-    rommap = [[]]
+    rommap = []
 
     with open(filepath, 'rb') as file:
-        readToAddr(file, rommap[0])
+        readFromBank(rommap, file)
 
-        if mismatchedHeaderTitle(header, rommap, 0xc0):
+        if mismatchedHeaderTitle(header, rommap, 0xffc0):
             print(f"This is not a {title} Game.")
             return
 
         for b in range(1, 0x28):
-            b_arr = []
-            readToAddr(file, b_arr)
-            rommap.append(b_arr)
+            readFromBank(rommap, file)
     
     return rommap
