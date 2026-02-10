1dbb8300551137e1609fac4ce036b54e727f7d78
BrainWave <pikadon92+1@gmail.com>
2026-02-10 23:27:14 +0800

    Complete Palettes to Write

--- tools/write.py
+++ tools/write.py
@@ -107,7 +107,15 @@ def palette(rommap):
     }
 
     for d in [
-        {DICT_FILE: "map", DICT_ADDR: 0xd340, DICT_BYTES: 40}
+        {DICT_FILE: "window", DICT_ADDR: 0xd340, DICT_ITEMS: 1, DICT_BYTES: 0x40},
+        {DICT_FILE: "map", DICT_ADDR: 0x3bb00, DICT_ITEMS: 256, DICT_BYTES: 44},
+        {DICT_FILE: "monster", DICT_ADDR: 0xed0000, DICT_ITEMS: 1, DICT_BYTES: 0x2270},
+        {DICT_FILE: "world", DICT_ADDR: 0xffcc0, DICT_ITEMS: 256, DICT_BYTES: 3},
+        {DICT_FILE: "attack", DICT_ADDR: 0x11a3a0, DICT_ITEMS: 16, DICT_BYTES: 128},
+        {DICT_FILE: "battle_char", DICT_ADDR: 0x14a3c0, DICT_ITEMS: 32, DICT_BYTES: 110},
+        {DICT_FILE: "battle_bg", DICT_ADDR: 0x14bb31, DICT_ITEMS: 32, DICT_BYTES: 84},
+        {DICT_FILE: "attack_target", DICT_ADDR: 0x199655, DICT_ITEMS: 1, DICT_BYTES: 0x1f0},
+        {DICT_FILE: "map_sprites", DICT_ADDR: 0x1ffc00, DICT_ITEMS: 32, DICT_BYTES: 32}
     ]:
         inner_f = {
             DICT_FUNC: writeByte,
