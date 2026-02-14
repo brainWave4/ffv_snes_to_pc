dc9474fcbcb96d29e028cea835820d70eb3d09b8
BrainWave <pikadon92+1@gmail.com>
2026-02-14 21:54:26 +0800

    Add Clearing Assets

---
+++ tools/clear_assets.py
@@ -0,0 +1,17 @@
+import os
+
+EXTS = ['.1bpp', '.2bpp', '.4bpp', '.8bpp', '.bin', '.pal', '.txt']
+
+def clear_in_file(path):
+    for entry in os.listdir(path):
+        fullpath = os.path.join(path, entry)
+
+        if os.path.isdir(fullpath):
+            clear_in_file(fullpath)
+        else:
+            ext = os.path.splitext(fullpath)[1]
+            if ext in EXTS:
+                os.remove(fullpath)
+                print("Cleared " + fullpath)
+
+clear_in_file("src")
