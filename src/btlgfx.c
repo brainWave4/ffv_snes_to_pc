d83f6a6d048a8277fee40c29149d7704d19cfb61
BrainWave <pikadon92+1@gmail.com>
2026-03-03 23:36:21 +0800

    Bank Battle Graphics up to _202F

--- src/btlgfx.c
+++ src/btlgfx.c
@@ -10,6 +10,106 @@
 // for ExecSound_ext
 #include "sound.h"
 
+static void func_c1003f(void); // Incomplete
+static void func_c10092(void); // Incomplete
+static void func_c100c6(void); // Incomplete
+static void func_c100f2(void); // Incomplete
+static void func_c1010b(void); // Incomplete
+static void func_c1014c(void); // Incomplete
+static void func_c10158(void); // Incomplete
+static void func_c1016c(void); // Incomplete
+static void func_c101e4(void); // Incomplete
+static void func_c101f6(void); // Incomplete
+static void func_c101fe(void); // Incomplete
+static void func_c10208(void); // Incomplete
+static void showDamageNumerals(void); // Incomplete
+static void func_c10276(void); // Incomplete
+static void func_c10293(void); // Incomplete
+static void func_c102ab(void); // Incomplete
+static void func_c102df(void); // Incomplete
+static void func_c102e0(void); // Incomplete
+static void func_c102f2(void); // Incomplete
+static void func_c102fe(void); // Incomplete
+static void func_c10348(void); // Incomplete
+static void updateSongTempoMult(void); // Incomplete
+static void updateReflectSfx(void); // Incomplete
+static void func_c10401(void); // Incomplete
+static void func_c10491(void); // Incomplete
+static void initRam(void); // Incomplete
+static void updateSavedCursorChecksums(void); // Incomplete
+static void validateSavedCursorChecksums(void); // Incomplete
+static void resetSavedCursorPos(void); // Incomplete
+static void calcSavedCursorChecksum(void); // Incomplete
+static void clearSavedCursorPos(void); // Incomplete
+static void getMonsterPalette(void); // Incomplete
+static void initHardwareRegisters(void); // Incomplete
+static void afterFunc_c10987(void); // Incomplete
+static void func_c109a7(void); // Incomplete
+static void func_c109b9(void); // Incomplete
+static void func_c109ca(void); // Incomplete
+static void func_c109d6(void); // Incomplete
+static void func_c109e5(void); // Incomplete
+static void func_c109f1(void); // Incomplete
+static void func_c10a00(void); // Incomplete
+static void func_c10a32(void); // Incomplete
+static void func_c10a6e(void); // Incomplete
+static void func_c10aea(void); // Incomplete
+static void func_c10b1b(void); // Incomplete
+static void func_c10b59(void); // Incomplete
+static void playQueuedSong(void); // Incomplete
+static void func_c10ba4(void); // Incomplete
+static void updateNeoExdeathBattleBg(void); // Incomplete
+static void func_c10f51(void); // Incomplete
+static void updateUnderwaterTowerWalseBg(void); // Incomplete
+static void updateDesertBg(void); // Incomplete
+static void func_c11015(void); // Incomplete
+static void func_c1105a(void); // Incomplete
+static void updateKarnakCastleBg(void); // Incomplete
+static void func_c1112f(void); // Incomplete
+static void func_c11186(void); // Incomplete
+static void func_c111c5(void); // Incomplete
+static void func_c111d3(void); // Incomplete
+static void func_c111e4(void); // Incomplete
+static void updateBgScrollMosiacRegs(void); // Incomplete
+static void func_c11235(void); // Incomplete
+static void func_c1124a(void); // Incomplete
+static void func_c112e8(void); // Incomplete
+static void copyBtlBgToVram(void); // Incomplete
+static void func_c1138e(void); // Incomplete
+static void func_c113da(void); // Incomplete
+static void func_c11434(void); // Incomplete
+static void func_c11482(void); // Incomplete
+static void func_c115d1(void); // Incomplete
+static void func_c1162c(void); // Incomplete
+static void func_c1169d(void); // Incomplete
+static void func_c116f2(void); // Incomplete
+static void func_c1170b(void); // Incomplete
+static void func_c117c4(void); // Incomplete
+static void func_c117d7(void); // Incomplete
+static void func_c117f0(void); // Incomplete
+static void func_c11815(void); // Incomplete
+static void func_c1187f(void); // Incomplete
+static void func_c118ae(void); // Incomplete
+static void func_c118d2(void); // Incomplete
+static void battleIrq(void); // Incomplete
+static void battleNmi(void); // Incomplete
+static void func_c11a57(void); // Incomplete
+static void func_c11aa6(void); // Incomplete
+static void func_c11b0d(void); // Incomplete
+static void func_c11b65(void); // Incomplete
+static void func_c11c84(void); // Incomplete
+static void func_c11c8a(void); // Incomplete
+static void blackWhiteMonsterPalette(void); // Incomplete
+static void restoreMonsterPalette(void); // Incomplete
+static void func_c11cdf(void); // Incomplete
+static void func_c11ceb(void); // Incomplete
+static void func_c11d25(void); // Incomplete
+static void func_c11d48(void); // Incomplete
+static void func_c11d65(void); // Incomplete
+static void func_c11df6(void); // Incomplete
+static void func_c11f22(void); // Incomplete
+static void func_c1202f(void); // Incomplete
+
 // Execute Battle Graphics
 void execBtlGfx(void) {}
 
@@ -18,3 +118,225 @@ void func_c10003(void) {}
 void func_c10006(void) {}
 
 void func_c10009(void) {}
+
+static void func_c1003f(void) {}
+
+static void func_c10092(void) {}
+
+static void func_c100c6(void) {}
+
+static void func_c100f2(void) {}
+
+static void func_c1010b(void) {}
+
+static void func_c1014c(void) {}
+
+static void func_c10158(void) {}
+
+static void func_c1016c(void) {}
+
+static void func_c101e4(void) {}
+
+static void func_c101f6(void) {}
+
+static void func_c101fe(void) {}
+
+static void func_c10208(void) {}
+
+// Address: _020d
+static void showDamageNumerals(void) {}
+
+static void func_c10276(void) {}
+
+static void func_c10293(void) {}
+
+static void func_c102ab(void) {}
+
+static void func_c102df(void) {}
+
+static void func_c102e0(void) {}
+
+static void func_c102f2(void) {}
+
+static void func_c102fe(void) {}
+
+static void func_c10348(void) {}
+
+// Address: _03a6
+static void updateSongTempoMult(void) {}
+
+// Address: _03f0
+static void updateReflectSfx(void) {}
+
+static void func_c10401(void) {}
+
+// Address: _0490 only Returns to SubRoutine
+
+static void func_c10491(void) {}
+
+// Address: _04c9
+static void initRam(void) {}
+
+// Address: _081c
+static void updateSavedCursorChecksums(void) {}
+
+// Address: _082c
+static void validateSavedCursorChecksums(void) {}
+
+// Address: _0847
+static void resetSavedCursorPos(void) {}
+
+// Address: _085a
+static void calcSavedCursorChecksum(void) {}
+
+// Address: _0871
+static void clearSavedCursorPos(void) {}
+
+// Address: _087f
+static void getMonsterPalette(void) {}
+
+// Address: _0888
+static void initHardwareRegisters(void) {}
+
+static void afterFunc_c10987(void) {}
+
+static void func_c109a7(void) {}
+
+static void func_c109b9(void) {}
+
+static void func_c109ca(void) {}
+
+static void func_c109d6(void) {}
+
+static void func_c109e5(void) {}
+
+static void func_c109f1(void) {}
+
+static void func_c10a00(void) {}
+
+static void func_c10a32(void) {}
+
+static void func_c10a6e(void) {}
+
+static void func_c10aea(void) {}
+
+static void func_c10b1b(void) {}
+
+static void func_c10b59(void) {}
+
+// Address: _0b81
+static void playQueuedSong(void) {}
+
+static void func_c10ba4(void) {}
+
+// Address: _0c5f
+static void updateNeoExdeathBattleBg(void) {}
+
+static void func_c10f51(void) {}
+
+// Address: _0fc3
+static void updateUnderwaterTowerWalseBg(void) {}
+
+// Address: _0fd4
+static void updateDesertBg(void) {}
+
+static void func_c11015(void) {}
+
+static void func_c1105a(void) {}
+
+// Address: _10eb
+static void updateKarnakCastleBg(void) {}
+
+static void func_c1112f(void) {}
+
+static void func_c11186(void) {}
+
+static void func_c111c5(void) {}
+
+static void func_c111d3(void) {}
+
+static void func_c111e4(void) {}
+
+// Address: _11f5
+static void updateBgScrollMosiacRegs(void) {}
+
+static void func_c11235(void) {}
+
+static void func_c1124a(void) {}
+
+static void func_c112e8(void) {}
+
+// Address: _133d
+static void copyBtlBgToVram(void) {}
+
+static void func_c1138e(void) {}
+
+static void func_c113da(void) {}
+
+static void func_c11434(void) {}
+
+static void func_c11482(void) {}
+
+static void func_c115d1(void) {}
+
+static void func_c1162c(void) {}
+
+static void func_c1169d(void) {}
+
+static void func_c116f2(void) {}
+
+static void func_c1170b(void) {}
+
+static void func_c117c4(void) {}
+
+static void func_c117d7(void) {}
+
+static void func_c117f0(void) {}
+
+static void func_c11815(void) {}
+
+static void func_c1187f(void) {}
+
+static void func_c118ae(void) {}
+
+static void func_c118d2(void) {}
+
+// Address: _19e9
+static void battleIrq(void) {}
+
+// Address: _19ea
+static void battleNmi(void) {}
+
+static void func_c11a57(void) {}
+
+static void func_c11aa6(void) {}
+
+static void func_c11b0d(void) {}
+
+static void func_c11b65(void) {}
+
+static void func_c11c84(void) {}
+
+static void func_c11c8a(void) {}
+
+// Address: _1cb1
+static void blackWhiteMonsterPalette(void) {}
+
+// Address: _1cb8
+static void restoreMonsterPalette(void) {}
+
+static void func_c11cdf(void) {}
+
+static void func_c11ceb(void) {}
+
+static void func_c11d25(void) {}
+
+static void func_c11d48(void) {}
+
+static void func_c11d65(void) {}
+
+static void func_c11df6(void) {}
+
+static void func_c11f22(void) {}
+
+static void func_c1202f(void) {}
