605def30f67e1788190bf39060b93687102542b1
BrainWave <pikadon92+1@gmail.com>
2026-03-07 21:23:41 +0800

    Bank Sound Functions

--- src/sound.c
+++ src/sound.c
@@ -1,12 +1,39 @@
 // Bank Range: C4
 #include "sound.h"
 
+static void playSong(void); // Incomplete
+static void otherInterrupts(void); // Incomplete
+static void interrupts70_7F(void); // Incomplete
+static void songNeedsSuspension(void); // Incomplete
+
+// Address: _002c
 // Initiate Sound
 void initSound(void) {
 
 }
 
+// Address: _014c
 // Execute Sound
 void execSound(void) {
+
+}
+
+// Address: _0188
+static void playSong(void) {
+
+}
+
+// Address: _0589
+static void otherInterrupts(void) {
+
+}
+
+// Address: _05c9
+static void interrupts70_7F(void) {
+
+}
+
+// Address: _05e0
+static void songNeedsSuspension(void) {
 
 }
