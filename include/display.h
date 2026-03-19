22cc020c459487c8c6dcb585f176ac1e3e0ec22e
BrainWave <pikadon92+1@gmail.com>
2026-03-19 14:32:43 +0800

    Add Background Layer

--- include/display.h
+++ include/display.h
@@ -1,3 +1,20 @@
+#ifndef SNES_DISPLAY
+#define SNES_DISPLAY
+
+#include <stdint.h>
+
+typedef struct {
+    // TODO: Get Tileset
+
+    uint16_t[1024] tilemap;
+
+    uint8_t tilemapCount;
+
+    uint8_t hScroll;
+
+    uint8_t vScroll;
+} BgLayer;
+
 // Address: _2100
 uint8_t inidisp;
 
@@ -6,3 +23,6 @@ uint8_t bgMode;
 
 // Address: _2106
 uint8_t mosaic;
+
+// Addresses _2115-2119 are VRAM
+#endif
